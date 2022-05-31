6e0bb007e18ce336462f0120ec959c113e30d032
unknown <tgantara@gmail.com>
2022-06-01 01:21:22 +0700

    update

--- resources/views/smart-city/nilai/js.blade.php
+++ resources/views/smart-city/nilai/js.blade.php
@@ -68,13 +68,12 @@
                         type: "GET",
                         url: `../kuisioner/${skpd}`,
                         success: function (res) {
-                            if(res.length > 0)
-                                {
-                                    $('#form-submit').attr('action', '../nilai');
-                                    $('#submit-data').text('Submit');
-                                    $('#submit-data').css('visibility', 'visible');
-                                    res.forEach(res => {
-                                        form = `
+                            if (res.length > 0) {
+                                $('#form-submit').attr('action', '../nilai');
+                                $('#submit-data').text('Submit');
+                                $('#submit-data').css('visibility', 'visible');
+                                res.forEach(res => {
+                                    form = `
                                             <div class="form-group row">
                                                 <input type="hidden" class="form-control" name="id_kuisioner[]" value="${res.id}">
                                                 <label class="col-sm-3 mt-2 col-form-label font-weight-bold">${res.kuisioner}</label>
@@ -87,39 +86,38 @@
                                                 </div>
                                                 <div class="form-group col-sm-2">
                                                     <label >Nilai</label>
-                                                    <input type="text" class="form-control"  placeholder="Masukkan Nilai" value="" name="nilai[]" id="nilai-${res.id}">
+                                                    <input type="text" class="form-control" oninvalid="this.setCustomValidity('Nilai Harus Diisi')"  placeholder="Masukkan Nilai" value="" name="nilai[]" id="nilai-${res.id}" required>
                                                 </div>
                                                 <div class="form-group col-sm-2">
                                                     <label>Unit Penyedia Data</label>
-                                                    <select class="form-control" name="penyedia[]" id="penyedia-${res.id}">
+                                                    <select class="form-control" name="penyedia[]" id="penyedia-${res.id}" required>
                                                     <option value="">Pilih</option>
-                                                    @foreach($skpd as $p)
+@foreach($skpd as $p)
                                                         <option value={{ $p->id }}>{{ $p->nama }}</option>
-                                                    @endforeach
+@endforeach
                                                     </select>
                                                 </div>
                                                 <div class="form-group col-sm-3">
                                                 <div class="form-group">
                                                     <label>Keterangan</label>
-                                                    <textarea class="form-control" rows="2" name="keterangan[]" id="keterangan-${res.id}"></textarea>
+                                                    <textarea class="form-control" rows="2" name="keterangan[]" id="keterangan-${res.id}" required></textarea>
                                                 </div>
                                                 </div>
                                             </div>
                                             `;
-                                        data.push(form)
-                                        $('#alert').html(`<div class="alert alert-warning" role="alert">
+                                    data.push(form)
+                                    $('#alert').html(`<div class="alert alert-warning" role="alert">
                                                 Belum Diisi
                                         </div>`);
-                                    });
-                                    $('#form-kuisioner').html(data);
-                                }
-                                else{
-                                    $('#submit-data').css('visibility', 'hidden');
-                                    $('#alert').html(`<div class="alert alert-warning" role="alert">
+                                });
+                                $('#form-kuisioner').html(data);
+                            } else {
+                                $('#submit-data').css('visibility', 'hidden');
+                                $('#alert').html(`<div class="alert alert-warning" role="alert">
                                                 Belum Ada
                                 </div>`);
                                 $('#form-kuisioner').html('');
-                                }
+                            }
                         },
                         error: function () {}
                     })
@@ -134,19 +132,25 @@
 
     function ketersediaanData(id, nilai, keterangan, penyedia_data) {
         let status = $(`#ketersediaan-${id}`).val();
-        if (status == 'tersedia') { 
-            $(`input[id=nilai-${id}`).attr('readonly', false);
-            $(`input[id=nilai-${id}`).val(nilai);
+        if (status == 'tersedia') {
+            $(`input[id=nilai-${id}]`).attr('readonly', false);
+            $(`input[id=nilai-${id}]`).attr('required', true);
+            $(`input[id=nilai-${id}]`).val(nilai);
             $(`select[id=penyedia-${id}]`).attr('readonly', false);
+            $(`select[id=penyedia-${id}]`).attr('required', true);
             $(`select[id=penyedia-${id}]`).val(penyedia_data);
             $(`textarea[id=keterangan-${id}]`).attr('readonly', false);
+            $(`textarea[id=keterangan-${id}]`).attr('required', true);
             $(`textarea[id=keterangan-${id}]`).val(keterangan);
         } else {
-            $(`input[id=nilai-${id}`).attr('readonly', true);
-            $(`input[id=nilai-${id}`).val('');
+            $(`input[id=nilai-${id}]`).attr('readonly', true);
+            $(`input[id=nilai-${id}]`).val('');
+            $(`input[id=nilai-${id}]`).attr('required', false);
             $(`select[id=penyedia-${id}]`).attr('readonly', true);
             $(`select[id=penyedia-${id}]`).val('');
+            $(`select[id=penyedia-${id}]`).attr('required', false);
             $(`textarea[id=keterangan-${id}]`).attr('readonly', true);
+            $(`textarea[id=keterangan-${id}]`).attr('required', false);
             $(`textarea[id=keterangan-${id}]`).val('');
         }
     }
